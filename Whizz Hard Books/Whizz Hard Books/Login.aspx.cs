8baf4c2382622f83b073fe496ebe4f6290911392
MikeBravoz <jbravozam@gmail.com>
2015-06-14 23:23:31 -0600

    Inicio Practica1
    se empezo a desarrollar la interfaz grafica, el diseño de nuestra pagina
    web.

---
+++ Whizz Hard Books/Whizz Hard Books/Login.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Whizz_Hard_Books
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
